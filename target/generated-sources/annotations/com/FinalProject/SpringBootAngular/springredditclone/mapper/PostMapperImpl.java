4e82700a0f3fd84c6ff349c9e9d95ab29bee8562
Kuan <brianpoo22471152@gmail.com>
2023-03-17 13:20:48 +0800

    testing ok

--- target/generated-sources/annotations/com/FinalProject/SpringBootAngular/springredditclone/mapper/PostMapperImpl.java
+++ target/generated-sources/annotations/com/FinalProject/SpringBootAngular/springredditclone/mapper/PostMapperImpl.java
@@ -10,8 +10,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-03-16T18:57:42+0800",
-    comments = "version: 1.5.3.Final, compiler: Eclipse JDT (IDE) 3.33.0.v20230213-1046, environment: Java 17.0.6 (Eclipse Adoptium)"
+    date = "2023-03-17T13:12:33+0800",
+    comments = "version: 1.5.3.Final, compiler: Eclipse JDT (IDE) 3.33.0.v20230218-1114, environment: Java 17.0.6 (Eclipse Adoptium)"
 )
 @Component
 public class PostMapperImpl extends PostMapper {
